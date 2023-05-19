a2feab7c3e0583bd0c1c12d1615278e638bc5b52
Kunal Dubey <kunal.d@Technovert.com>
2023-05-19 22:54:00 +0530

    added abstract class

--- DecoratorPattern.ts
+++ DecoratorPattern.ts
@@ -1,42 +1,53 @@
-class Pizza {
-  public Toppings: Topping[];
-  public Name: string;
-  public Type: Food;
-  public Cost : number;
+abstract class BasePizza {
+    public Toppings: Topping[];
+    public Name: string;
+    public Type: boolean;
+    public Cost: number; 
 
-  constructor(name: string, type: Food, cost: number) {
-    this.Name = name;
-    this.Type = type;
-    this.Toppings = [];
-    this.Cost = cost;
-  }
-
-  addDiscount(discount: number) {
-    if (this.Cost < discount) {
-      this.Cost = 0;
-    } else {
-      this.Cost = this.Cost - discount;
+    constructor(name: string, type: boolean, cost: number) {
+        this.Name = name;
+        this.Type = type;
+        this.Toppings = [];
+        this.Cost = cost;
     }
-  }
 
-  printToppings(){
-    this.Toppings.forEach(topping => console.log(`${topping.Name} :`, `${topping.Type}`))
-  }
+    addDiscount(discount: number) {
+        if (this.Cost < discount) {
+          this.Cost = 0;
+        } else {
+          this.Cost = this.Cost - discount;
+        }
+      }
+    
+      printToppings() {
+        this.Toppings.forEach((topping) =>
+          console.log(`${topping.Name} :`, `${topping.Type}`)
+        );
+      }
+    
+      getFoodType = (type: boolean) => (type ? "VEG" : "NON-VEG");
+    
+      getDescription() {
+        console.log("Pizza Name :", this.Name),
+          console.log("Toppings : "),
+          this.printToppings();
+        console.log("Total Cost: ", this.Cost);
+        console.log("Pizza Type : ", this.getFoodType(this.Type));
+      }
 
-  getDescription(){
-    console.log("Pizza Name :", this.Name),
-    console.log("Toppings : "), 
-    this.printToppings();
-    console.log("Total Cost: ", this.Cost);
-  }
 }
 
-type Food = "VEG" | "NON-VEG";
+
+class Pizza extends BasePizza {
+  constructor(name: string, type: boolean, cost: number) {
+    super(name, type, cost);
+  }
+} 
 
 type Topping = {
   Name: string;
   Cost: number;
-  Type: Food;
+  Type: boolean;
 };
 
 const toppingCost: number = 100;
@@ -50,11 +61,7 @@ class PizzaToppingDecorator {
     this._topping = topping;
     this._basePizza.Toppings.push(this._topping);
     this._basePizza.Cost += this._topping.Cost;
-    if (this._basePizza.Type == "VEG") {
-      if (this._topping.Type == "NON-VEG") {
-        this._basePizza.Type = "NON-VEG";
-      }
-    }
+    this._basePizza.Type = this._basePizza.Type && this._topping.Type;
   }
 
   add() {
@@ -65,22 +72,23 @@ class PizzaToppingDecorator {
 const MushroomTopping: Topping = {
   Name: "Mushroom",
   Cost: 70,
-  Type: "VEG",
+  Type: true,
 };
 
 const PepperoniTopping: Topping = {
   Name: "Pepperoni",
   Cost: 90,
-  Type: "NON-VEG",
+  Type: false,
 };
 
 const ExtraCheeseTopping: Topping = {
   Name: "ExtraCheese",
   Cost: 90,
-  Type: "VEG",
+  Type: true,
 };
 
-let newPizza: Pizza = new Pizza("Margharita", "VEG", 200);
+
+let newPizza: Pizza = new Pizza("Margharita", true, 200);
 newPizza = new PizzaToppingDecorator(newPizza, MushroomTopping).add();
 newPizza = new PizzaToppingDecorator(newPizza, PepperoniTopping).add();
 newPizza.addDiscount(40);
